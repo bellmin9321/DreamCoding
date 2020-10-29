f141818e217dd6d751047c92c6d3222ce620ad68
bellmin9321 <bellmin9321@gmail.com>
2020-10-29 12:14:00 +0900

    add practice js

--- chapter2/shoppingList/pr.js
+++ chapter2/shoppingList/pr.js
@@ -1,50 +1,59 @@
-const toDoForm = document.querySelector(".send-text"),
-  toDoInput = toDoForm.querySelector("input"),
-  toDoList = document.querySelector(".todolist");
+const input = document.querySelector('.footer__input')
+const addBtn = document.querySelector('.footer__button')
+const items = document.querySelector('.items')
 
-const TODOS_LS = "toDos";
-
-let toDos = [];
-
-function saveToDos() {
-  localStorage.setItem(TODOS_LS, toDos)
+function onAdd() {
+  const text = input.value;
+  if(text === ''){
+    input.focus();
+    return;
+  }
+  console.log(text);
+  
+  const item = createItem(text);
+  // createItem = 밑에 함수
+  items.appendChild(item);
+  items.scrollIntoView({block: 'center'});
+  input.value = '';
+  input.focus();
 }
 
-function showToDos(text) {
-  const li = document.createElement('li')
-  const delBtn = document.createElement('button')
-  li.innerText = text;
-  delBtn.innerText = "X"
-  li.id = toDos.length + 1;
-  delBtn.addEventListener("click", handleDelete)
-  li.appentChild(delBtn)
-  toDoList.appendChild(li)
-  toDoInput.value = "";
-  const toDoObj = {
-    text: text,
-    id: toDos.length + 1,
-  };
-  toDos.push(toDoObj);
-  saveToDos()
+function createItem (text) {
+  const itemRow = document.createElement('li')
+  itemRow.setAttribute('class', 'item__Row')
+
+  const item = document.createElement('div');
+  item.setAttribute('class', 'item')
+
+  const name = document.createElement('span');
+  name.setAttribute('class', 'item__name')
+  name.innerText = text;
+
+  const deleteBtn = document.createElement('button')
+  deleteBtn.setAttribute('class', 'item__delete')
+  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>'
+  deleteBtn.addEventListener('click', () => {
+    items.removeChild(itemRow)
+  })
+
+  const itemDivider = document.createElement('div');
+  itemDivider.setAttribute('class', 'item__divider');
+
+  item.appendChild(name);
+  item.appendChild(deleteBtn);
+
+  itemRow.appendChild(item);
+  itemRow.appendChild(itemDivider);
+  return itemRow;
+
 }
 
-function handleSubmit() {
-  const currentValue = toDoInput.value;
-  showToDos(currentValue)
-}
+addBtn.addEventListener('click', () => {
+  onAdd();
+})
 
-function loadList () {
-  const loadtoDos = locallStorage.getItem(TODOS_LS)
-  toDoForm.addEventListener("submit", handleSubmit)
-  if (loadToDos !== null ){
-    const parseToDo = JSON.parse(loadToDos);
-    parseToDo.forEach(function (toDo) {
-      showToDos(toDo.text)
-    })
-  } 
-}
-
-function show() {
-  loadList();
-}
-show()
+input.addEventListener('keypress', (event) => {
+  if(event.key == "Enter") {
+    onAdd();
+  }
+})
