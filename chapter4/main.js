64d1f9b2d7ba73fd9be26b019fb8960f3f6ccea6
bellmin9321 <bellmin9321@gmail.com>
2020-11-03 15:31:12 +0900

    add main.js

---
+++ chapter4/main.js
@@ -0,0 +1,15 @@
+const start = document.querySelector('.start');
+const timer = document.querySelector('.timer');
+const rest = document.querySelector('.rest');
+const carrot = document.querySelector('.carrot');
+const bug = document.querySelector('.bug');
+
+function deleteItem() {
+  const
+}
+
+start.addEventListener('click', (event) => {
+  if(event.target.) {
+
+  } 
+})
