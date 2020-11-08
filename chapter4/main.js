757934ad074763ea6441c5d2f1272a8ebd2f862d
bellmin9321 <bellmin9321@gmail.com>
2020-11-08 17:50:02 +0900

    add timer but not working

--- chapter4/main.js
+++ chapter4/main.js
@@ -4,12 +4,35 @@ const rest = document.querySelector('.rest');
 const carrot = document.querySelector('.carrot');
 const bug = document.querySelector('.bug');
 
-function deleteItem() {
-  const
-}
+/*
+쉬운것부터 작업
+1. start 버튼 누르면 || 로 바뀌기 > 타이머 시작
+2. 
 
-start.addEventListener('click', (event) => {
-  if(event.target.) {
+*/
 
-  } 
-})
+// function deleteItem() {
+//   const
+// }
+
+// start.addEventListener('click', (event) => {
+//   if(event.target.) {
+
+//   } 
+// })
+
+
+
+const x = setInterval(function() {
+  const time = 60;
+  const min = ""
+  const sec = ""
+  min = parseInt(time/60);
+  sec = time%60;
+  document.querySelector('.timer').innerHTML = min + ":" + sec + time--;
+
+  if ( time < 0 ) {
+    clearInterval(x);
+    document.querySelector('.timer').innerHTML = "TIME OVER";
+  }
+}, 1000);
