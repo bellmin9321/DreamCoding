3e1705d9b1b3b0c19353a8221690e673665e2ab7
bellmin9321 <bellmin9321@gmail.com>
2020-11-10 23:16:13 +0900

    setInterval 다른방법

--- chapter4/main.js
+++ chapter4/main.js
@@ -21,21 +21,40 @@ const bug = document.querySelector('.bug');
 //   } 
 // })
 
+// const x = setInterval(function() {
+//   const time = 60;
+//   const min = ""
+//   const sec = ""
+//   min = parseInt(time/60);
+//   sec = time%60;
+//   document.querySelector('.timer').innerHTML = min + ":" + sec + time--;
 
+//   if ( time < 0 ) {
+//     clearInterval(x);
+//     document.querySelector('.timer').innerHTML = "TIME OVER";
+//   }
+// }, 1000);
 
-const x = setInterval(function() {
-  const time = 60;
-  const min = ""
-  const sec = ""
-  min = parseInt(time/60);
-  sec = time%60;
-  document.querySelector('.timer').innerHTML = min + ":" + sec + time--;
+const count = 60;
+const time = 0;
+const min = ""
+const sec = ""
+min = parseInt(time/60);
+sec = time%60;
+document.querySelector('.timer').innerHTML = "TIME OVER";
+function start() {
+  clearInterval(time);
+  time = setInterval("func()", 1000);
+}
 
-  if ( time < 0 ) {
-    clearInterval(x);
-    document.querySelector('.timer').innerHTML = "TIME OVER";
+function func() {
+  count = count + 1;
+  if(count != 0) {
+
+  } else if (count = 0) {
+    clearInterval(time);
   }
-}, 1000);
+}
 
 // setInterval vs setTimeout
 // 특정 시간 이후 단 한 번만 특정 함수 또는 코드를 실행
